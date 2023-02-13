179da61e934f4830a61671d0ba8ff7c0e49f014a
Gius993 <giuseppevidili9@gmail.com>
2023-02-13 16:12:34 +0100

    bootstrap e link pag

--- bestoftheyearp/src/main/java/com/example/demo/controller/MainController.java
+++ bestoftheyearp/src/main/java/com/example/demo/controller/MainController.java
@@ -7,8 +7,8 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 
 import com.classes.model.Movie;
 import com.classes.model.Song;
@@ -17,41 +17,45 @@ import com.classes.model.Song;
 @RequestMapping("/")
 
 public class MainController {
+
 	
-	@GetMapping()	
+	@GetMapping()
 	public String index(Model model) {
-		String name = "Best of the year by " + "Giuseppe";
-		model.addAttribute("name", name);
+		String title = "Best of the year by Giuseppe";
+		model.addAttribute("title", title);
 		return "index";
 	}
-	 private List<Movie> getBestMovies() {
-	        List<Movie> bestMovies = new ArrayList<>();
-	        bestMovies.add(new Movie(1, "Il sempre nato"));
-	        bestMovies.add(new Movie(2, "Il padrino"));
-	        bestMovies.add(new Movie(3, "Il padrino pt2"));
-	        return bestMovies;
-	    }
-	 @GetMapping("/movies")
-	    public String getMovies(Model model) {
-	        List<Movie> bestMovies = getBestMovies();
-	        List<String> movieTitles = bestMovies.stream().map(Movie::getTitle).collect(Collectors.toList());
-	        model.addAttribute("movies", String.join(", ", movieTitles));
-	        return "movies";
-	    }
-	 
-	  private List<Song> getBestSongs() {
-	        List<Song> bestSongs = new ArrayList<>();
-	        bestSongs.add(new Song(1, "lalalaal"));
-	        bestSongs.add(new Song(2, "la sonatona"));
-	        bestSongs.add(new Song(3, "parapapa"));
-	        return bestSongs;
-	    }
 
-	  @GetMapping("/songs")
-	    public String getSongs(Model model) {
-	        List<Song> bestSongs = getBestSongs();
-	        List<String> songTitles = bestSongs.stream().map(Song::getTitle).collect(Collectors.toList());
-	        model.addAttribute("songs", String.join(", ", songTitles));
-	        return "songs";
-	    }
+	private List<Movie> getBestMovies() {
+		List<Movie> bestMovies = new ArrayList<>();
+		bestMovies.add(new Movie(1, "Il sempre nato"));
+		bestMovies.add(new Movie(2, "Il padrino"));
+		bestMovies.add(new Movie(3, "Il padrino pt2"));
+		return bestMovies;
+	}
+
+	@GetMapping("/movies")
+	public String movies(Model model) {
+		model.addAttribute("movies", getBestMovies());
+		return "movies";
+	}
+	// prendo film per id 
+	
+
+	
+	private List<Song> getBestSongs() {
+		List<Song> bestSongs = new ArrayList<>();
+		bestSongs.add(new Song(1, "lalalaal"));
+		bestSongs.add(new Song(2, "la sonatona"));
+		bestSongs.add(new Song(3, "parapapa"));
+		return bestSongs;
+	}
+
+	@GetMapping("/songs")
+	public String songs(Model model) {
+		model.addAttribute("songs", getBestSongs());
+		return "songs";
+	}
+
+
 }
